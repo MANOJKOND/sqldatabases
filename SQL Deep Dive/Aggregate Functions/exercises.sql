bedcabf910fab90007ddf8568c8142260b860e32
Manoj Konda <kondamanoj9989@gmail.com>
2025-08-03 10:17:48 +0530

    Update exercises.sql
    with answers

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -9,10 +9,13 @@
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
 
+select avg(salary) As "Average Salary" from salaries;
+
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
 
+SELECT max(birth_date) as Min_BirthDate from employees;
 
 -- BEFORE YOU START
 /*
@@ -23,7 +26,7 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
-
+select count(towns) as "towns" from towns;
 
 -- BEFORE YOU START
 /*
@@ -34,11 +37,14 @@
 
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
+SELECT count(countrycode) from countrylanguage
+WHERE isofficial = true;
 
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
+select avg(lifeexpectancy) from country;
 
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
-
-
+select avg(population) from city
+where countrycode = 'NLD';
